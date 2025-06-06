bc05a4de663dd526faf0eb124faecece50969d60
Grégoire Pineau <lyrixx@lyrixx.info>
2025-06-06 15:20:41 +0200

    removed no needed things

--- importmap.php
+++ importmap.php
@@ -16,13 +16,4 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
 ];
