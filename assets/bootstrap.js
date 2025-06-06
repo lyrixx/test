7c8e95df7581c5345b7643ece2185688a7ca06f2
Grégoire Pineau <lyrixx@lyrixx.info>
2025-06-06 14:58:14 +0200

    Add reproducer

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,5 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+// import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-const app = startStimulusApp();
+// const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
